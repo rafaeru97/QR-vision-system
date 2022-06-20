71873d18c19ecac695689b428a84f5b9a6538467
sqadi <sqadiam@gmail.com>
2022-06-21 01:52:36 +0200

    Dodaj pliki projektów.

---
+++ Robotyka 1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Robotyka_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 62);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(671, 59);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(640, 480);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original Photo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(977, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ROI";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(458, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 26);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Manual ROI";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(940, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 26);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Auto ROI";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(554, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(98, 26);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Display ROI";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1323, 554);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "ROI App";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Label label2;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+    }
+}
---
+++ Robotyka 1/Form1.cs
@@ -0,0 +1,224 @@
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System.Numerics;
+
+namespace Robotyka_1
+{
+    public partial class Form1 : Form
+    {
+        Rectangle rect;
+        Point StartROI, EndROI;
+        bool Selecting, MouseDown;
+        Image<Bgr, Byte> PrimeImage;
+        Image<Bgr, Byte> BinaryImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(Selecting)
+            {
+                MouseDown = true;
+                StartROI = e.Location;
+            }
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(Selecting)
+            {
+                int width = Math.Max(StartROI.X, e.X) - Math.Min(StartROI.X, e.X);
+                int height = Math.Max(StartROI.Y, e.Y) - Math.Min(StartROI.Y, e.Y);
+                rect = new Rectangle(Math.Min(StartROI.X, e.X), Math.Min(StartROI.Y, e.Y), width, height);
+                Refresh();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if(MouseDown)
+            {
+                using (Pen pen = new Pen(Color.Red, 3))
+                {
+                    e.Graphics.DrawRectangle(pen, rect);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if(Selecting)
+            {
+                Selecting = false;
+                MouseDown = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (pictureBox1.Image == null)
+                    return;
+
+                if (rect == Rectangle.Empty)
+                    return;
+
+                //var img = new Bitmap(pictureBox1.Image).ToImage<Bgr, byte>();
+                var img = PrimeImage.Resize(640, 480, Emgu.CV.CvEnum.Inter.Linear);
+                img.ROI = rect;
+
+                var imgROI = img.Copy();
+                img.ROI = Rectangle.Empty;
+
+                pictureBox2.Image = imgROI.ToBitmap();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+                return;
+
+            var Image = PrimeImage;
+            Image<Gray, Byte> TempImage;
+            TempImage = Image.Convert<Gray, Byte>();
+            
+            var corners = new Mat();
+
+            QRCodeDetector qrCodeDetector = new QRCodeDetector();
+            bool result = qrCodeDetector.DetectMulti(TempImage, corners);
+            
+            if (result)
+            {
+                var resultString =  new VectorOfCvString();
+                qrCodeDetector.DecodeMulti(TempImage, corners, resultString);
+
+                if (resultString.Length/32 != 4)
+                {
+                    MessageBox.Show("Not found 4 QR codes.");
+                    return;
+                }
+
+                var qr1_x1 = corners.GetData().GetValue(0, 0, 0);
+                var qr1_y1 = corners.GetData().GetValue(0, 0, 1);
+                var qr1_x2 = corners.GetData().GetValue(0, 2, 0);
+                var qr1_y2 = corners.GetData().GetValue(0, 2, 1);
+
+                var qr2_x1 = corners.GetData().GetValue(1, 0, 0);
+                var qr2_y1 = corners.GetData().GetValue(1, 0, 1);
+                var qr2_x2 = corners.GetData().GetValue(1, 2, 0);
+                var qr2_y2 = corners.GetData().GetValue(1, 2, 1);
+
+                var qr3_x1 = corners.GetData().GetValue(2, 0, 0);
+                var qr3_y1 = corners.GetData().GetValue(2, 0, 1);
+                var qr3_x2 = corners.GetData().GetValue(2, 2, 0);
+                var qr3_y2 = corners.GetData().GetValue(2, 2, 1);
+
+                var qr4_x1 = corners.GetData().GetValue(3, 0, 0);
+                var qr4_y1 = corners.GetData().GetValue(3, 0, 1);
+                var qr4_x2 = corners.GetData().GetValue(3, 2, 0);
+                var qr4_y2 = corners.GetData().GetValue(3, 2, 1);
+
+                int qr1_width = Math.Max(Convert.ToInt32(qr1_x1), Convert.ToInt32(qr1_x2)) - Math.Min(Convert.ToInt32(qr1_x1), Convert.ToInt32(qr1_x2));
+                int qr1_height = Math.Max(Convert.ToInt32(qr1_y1), Convert.ToInt32(qr1_y2)) - Math.Min(Convert.ToInt32(qr1_y1), Convert.ToInt32(qr1_y2));
+
+                int qr2_width = Math.Max(Convert.ToInt32(qr2_x1), Convert.ToInt32(qr2_x2)) - Math.Min(Convert.ToInt32(qr2_x1), Convert.ToInt32(qr2_x2));
+                int qr2_height = Math.Max(Convert.ToInt32(qr2_y1), Convert.ToInt32(qr2_y2)) - Math.Min(Convert.ToInt32(qr2_y1), Convert.ToInt32(qr2_y2));
+
+                int qr3_width = Math.Max(Convert.ToInt32(qr3_x1), Convert.ToInt32(qr3_x2)) - Math.Min(Convert.ToInt32(qr3_x1), Convert.ToInt32(qr3_x2));
+                int qr3_height = Math.Max(Convert.ToInt32(qr3_y1), Convert.ToInt32(qr3_y2)) - Math.Min(Convert.ToInt32(qr3_y1), Convert.ToInt32(qr3_y2));
+
+                int qr4_width = Math.Max(Convert.ToInt32(qr4_x1), Convert.ToInt32(qr4_x2)) - Math.Min(Convert.ToInt32(qr4_x1), Convert.ToInt32(qr4_x2));
+                int qr4_height = Math.Max(Convert.ToInt32(qr4_y1), Convert.ToInt32(qr4_y2)) - Math.Min(Convert.ToInt32(qr4_y1), Convert.ToInt32(qr4_y2));
+
+                Rectangle qr1_rect = new Rectangle(Math.Min(Convert.ToInt32(qr1_x1), Convert.ToInt32(qr1_x2)), Math.Min(Convert.ToInt32(qr1_y1), Convert.ToInt32(qr1_y2)), qr1_width, qr1_height);
+                Rectangle qr2_rect = new Rectangle(Math.Min(Convert.ToInt32(qr2_x1), Convert.ToInt32(qr2_x2)), Math.Min(Convert.ToInt32(qr2_y1), Convert.ToInt32(qr2_y2)), qr2_width, qr2_height);
+                Rectangle qr3_rect = new Rectangle(Math.Min(Convert.ToInt32(qr3_x1), Convert.ToInt32(qr3_x2)), Math.Min(Convert.ToInt32(qr3_y1), Convert.ToInt32(qr3_y2)), qr3_width, qr3_height);
+                Rectangle qr4_rect = new Rectangle(Math.Min(Convert.ToInt32(qr4_x1), Convert.ToInt32(qr4_x2)), Math.Min(Convert.ToInt32(qr4_y1), Convert.ToInt32(qr4_y2)), qr2_width, qr4_height);
+
+                Point qr1_center = new Point(((Convert.ToInt32(corners.GetData().GetValue(0, 2, 0)) - Convert.ToInt32(corners.GetData().GetValue(0, 0, 0))) / 2 + Convert.ToInt32(corners.GetData().GetValue(0, 0, 0))),
+                                             ((Convert.ToInt32(corners.GetData().GetValue(0, 2, 1)) - Convert.ToInt32(corners.GetData().GetValue(0, 0, 1))) / 2 + Convert.ToInt32(corners.GetData().GetValue(0, 0, 1)))
+                                             );
+
+                Point qr2_center = new Point(((Convert.ToInt32(corners.GetData().GetValue(1, 2, 0)) - Convert.ToInt32(corners.GetData().GetValue(1, 0, 0))) / 2 + Convert.ToInt32(corners.GetData().GetValue(1, 0, 0))),
+                                             ((Convert.ToInt32(corners.GetData().GetValue(1, 2, 1)) - Convert.ToInt32(corners.GetData().GetValue(1, 0, 1))) / 2 + Convert.ToInt32(corners.GetData().GetValue(1, 0, 1)))
+                                             );
+
+                Point qr3_center = new Point(((Convert.ToInt32(corners.GetData().GetValue(2, 2, 0)) - Convert.ToInt32(corners.GetData().GetValue(2, 0, 0))) / 2 + Convert.ToInt32(corners.GetData().GetValue(2, 0, 0))),
+                                              ((Convert.ToInt32(corners.GetData().GetValue(2, 2, 1)) - Convert.ToInt32(corners.GetData().GetValue(2, 0, 1))) / 2 + Convert.ToInt32(corners.GetData().GetValue(2, 0, 1)))
+                                              );
+
+                Point qr4_center = new Point(((Convert.ToInt32(corners.GetData().GetValue(3, 2, 0)) - Convert.ToInt32(corners.GetData().GetValue(3, 0, 0))) / 2 + Convert.ToInt32(corners.GetData().GetValue(3, 0, 0))),
+                                              ((Convert.ToInt32(corners.GetData().GetValue(3, 2, 1)) - Convert.ToInt32(corners.GetData().GetValue(3, 0, 1))) / 2 + Convert.ToInt32(corners.GetData().GetValue(3, 0, 1)))
+                                              );
+
+                int grROI_X_min = Math.Min(qr4_center.X, Math.Min(qr3_center.X, Math.Min(qr1_center.X, qr2_center.X)));
+                int grROI_Y_min = Math.Min(qr4_center.Y, Math.Min(qr3_center.Y, Math.Min(qr1_center.Y, qr2_center.Y)));
+                int grROI_X_max = Math.Max(qr4_center.X, Math.Max(qr3_center.X, Math.Max(qr1_center.X, qr2_center.X)));
+                int grROI_Y_max = Math.Max(qr4_center.Y, Math.Max(qr3_center.Y, Math.Max(qr1_center.Y, qr2_center.Y)));
+
+                int qrROI_width = grROI_X_max - grROI_X_min;
+                int qrROI_height = grROI_Y_max - grROI_Y_min;
+
+                Rectangle qrROI_rect = new Rectangle(grROI_X_min, grROI_Y_min, qrROI_width, qrROI_height);
+
+                Pen ROI_pen = new Pen(Color.Orange, 3);
+                pictureBox1.CreateGraphics().DrawRectangle(ROI_pen, qrROI_rect);
+
+                MCvScalar QRcolor = new MCvScalar(0, 255, 255);
+                MCvScalar QRcolorText = new MCvScalar(255, 0, 255);
+                MCvScalar ROIcolor = new MCvScalar(200, 0, 200);
+                Emgu.CV.CvEnum.FontFace Font = new Emgu.CV.CvEnum.FontFace();
+
+                Image<Bgr, Byte> DrawImage;
+                DrawImage = Image.Copy();
+
+                CvInvoke.Rectangle(DrawImage, qr1_rect, QRcolor, 5);
+                CvInvoke.Rectangle(DrawImage, qr2_rect, QRcolor, 5);
+                CvInvoke.Rectangle(DrawImage, qr3_rect, QRcolor, 5);
+                CvInvoke.Rectangle(DrawImage, qr4_rect, QRcolor, 5);
+                CvInvoke.Rectangle(DrawImage, qrROI_rect, ROIcolor, 5);
+                CvInvoke.PutText(DrawImage, "[1](" + qr1_center.X + "," + qr1_center.Y + ")", qr1_center, Font, 1, QRcolorText, 4);
+                CvInvoke.PutText(DrawImage, "[2](" + qr2_center.X + "," + qr2_center.Y + ")", qr2_center, Font, 1, QRcolorText, 4);
+                CvInvoke.PutText(DrawImage, "[3](" + qr3_center.X + "," + qr3_center.Y + ")", qr3_center, Font, 1, QRcolorText, 4);
+                CvInvoke.PutText(DrawImage, "[4](" + qr4_center.X + "," + qr4_center.Y + ")", qr4_center, Font, 1, QRcolorText, 4);
+
+                Image.ROI = qrROI_rect;
+
+                var imgROI = Image.Copy();
+                Image.ROI = Rectangle.Empty;
+
+                pictureBox1.Image = DrawImage.ToBitmap();
+                pictureBox2.Image = imgROI.ToBitmap();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try {
+                string path = @"C:\Users\sqadi\Desktop\Studia\obraz.jpg";
+                PrimeImage = new Image<Bgr, Byte>(path);
+                pictureBox1.Image = PrimeImage.ToBitmap();
+            }
+            catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Selecting = true;
+        }
+    }
+}
